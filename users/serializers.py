1720daa908992f3de1773680951a556c44c48201
meodiemvuong <thanhbinh16092k1@gmail.com>
2022-04-01 23:10:39 +0700

    hello
    api

---
+++ users/serializers.py
@@ -0,0 +1,18 @@
+from pyexpat import model
+from rest_framework import serializers
+from users.models import User
+
+class UserSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'phone', 'email' ]
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
